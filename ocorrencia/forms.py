216e2b26aa983ab82700d265fc4ddcbd997ded16
Franklin <franklinamaralt@gmail.com>
2022-07-04 22:37:56 -0300

    atualizando os forms

--- ocorrencia/forms.py
+++ ocorrencia/forms.py
@@ -1,34 +1,73 @@
 from dataclasses import fields
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.hashers import make_password
+
+from django.contrib.auth.forms import UserCreationForm
 
 from ocorrencia.consts import TipoCentroChoices, TipoOcorrenciaChoices
 
-from .models import Ocorrencia, Usuario
-class UsuarioCreateForm(UserCreationForm):
+from .models import Management, Ocorrencia
+
+class CreateManagementUserForm(forms.ModelForm):
+    
+
+    def clean(self):
+        if self.cleaned_data['password'] != self.cleaned_data['password2']:
+            self.add_error('password','Senhas Não Condizem!')
+        else:
+            password = self.cleaned_data.get('password')
+            if password:
+                self.cleaned_data['password'] =  make_password(password)
+            else:
+                self.add_error('password','Campo obrigatório!')
+        return super().clean()
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Repetir Senha",
+                "class": "form-control"
+            }
+        ))
 
     class Meta:
-        model = Usuario
-        fields = '__all__'
-        labels = {'username': 'Username/CPF'}
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password1'])
-        user.cpf = self.cleaned_data['username']
-        if commit:
-            user.save()
-        return user
-
-class UsuarioChangeForm(UserChangeForm):
-
-    class Meta:
-        model = Usuario
-        fields = '__all__'
+        model = Management
+        fields = ['username','first_name', 'email','office','parent', 'password', 'password2']
+        
+        widgets = {
+            'username':forms.TextInput(
+                    attrs={
+                        "placeholder": "Login",
+                        "class": "form-control"
+                    }),
+            'first_name':forms.TextInput(
+                    attrs={
+                        "placeholder": "Nome",
+                        "class": "form-control"
+                    }),
+            'email':forms.EmailInput(
+                    attrs={
+                        "placeholder": "Email",
+                        "class": "form-control",
+                    }),
+            'office':forms.Select(
+                    attrs={
+                        "class": "form-control",
+                    }),
+            'parent':forms.Select(
+                    attrs={
+                        "class": "form-control",
+                    }),
+            'password':forms.PasswordInput(
+                attrs={
+                    "placeholder": "Senha",
+                    "class": "form-control"
+                })
+        }
 
 
 class LoginUsuarioForm(forms.Form):
-    username = forms.CharField(max_length=11)
+    username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
 
 class FiltroRelatorioOcorrencia(forms.Form):
@@ -115,3 +154,37 @@ class LoginForm(forms.Form):
                 "class": "form-control"
             }
         ))
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control"
+            }
+        ))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "Email",
+                "class": "form-control"
+            }
+        ))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password check",
+                "class": "form-control"
+            }
+        ))
+
+    class Meta:
+        model = Management
+        fields = ('username', 'email', 'password1', 'password2')
